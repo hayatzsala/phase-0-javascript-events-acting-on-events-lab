d4db47d7b48454ee93b3713aa63112f24e660454
hayatSalawdeh <salawdeh.hayat@gmail.com>
2022-08-31 01:34:39 +0300

    done

--- index.js
+++ index.js
@@ -4,37 +4,51 @@ block.style.backgroundColor = '#FF69B4';
 
 document.addEventListener('keydown', (event)=>{
     if(event.key === 'ArrowLeft'){
-        const leftPos = block.style.left.replace("px","");
-        const left = parseInt(leftPos,10);
-        if(left>0){
-            block.style.left = `${left-1}px`;
-        }
+       moveDodgerLeft();
         
     }
     else if(event.key === "ArrowRight"){
-        const rightPos = block.style.left.replace("px","");
-        const right = parseInt(rightPos,10);
-        if(right<360){
-            block.style.left = `${right+1}px`;
-        }
+        moveDodgerRight();
         
     }
     else if(event.key === "ArrowUp"){
-        const topPos = block.style.bottom.replace("px","");
-        const top = parseInt(topPos,10);
-        if(top < 380){
-            block.style.bottom = `${top+1}px`;
-        }
+        moveDodgerUp();
        
     }
     else if(event.key === "ArrowDown"){
-        const downPos = block.style.bottom.replace("px","");
-        const down = parseInt(downPos,10);
-        if(down > 0){
-            block.style.bottom = `${down-1}px`;
-        }
+        moveDodgerDown();
         
     }
 
 });
+function moveDodgerLeft(){
+    const leftPos = block.style.left.replace("px","");
+    const left = parseInt(leftPos,10);
+    if(left>0){
+        block.style.left = `${left-1}px`;
+    }
+}
 
+function moveDodgerRight(){
+    const rightPos = block.style.left.replace("px","");
+    const right = parseInt(rightPos,10);
+    if(right<360){
+        block.style.left = `${right+1}px`;
+    }
+}
+
+function moveDodgerUp(){
+    const topPos = block.style.bottom.replace("px","");
+    const top = parseInt(topPos,10);
+    if(top < 380){
+        block.style.bottom = `${top+1}px`;
+    }
+}
+
+function moveDodgerDown(){
+    const downPos = block.style.bottom.replace("px","");
+    const down = parseInt(downPos,10);
+    if(down > 0){
+        block.style.bottom = `${down-1}px`;
+    }
+}
